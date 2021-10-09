bd551cca4209865b5df717f0c6ac19074faf6450
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2021-10-09 16:41:17 -0300

    .

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var clientRouter = require('./routes/client');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const open = require('open');
+
+open('http://localhost:3000', {app: 'chrome'});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/client', clientRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
